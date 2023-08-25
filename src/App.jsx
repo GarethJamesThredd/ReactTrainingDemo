53bbd153f66698c86fc405da84eb28ca3de0fe68
Mehmet Demircan <mehmet.demircan@thredd.com>
2023-08-25 14:46:56 +0100

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WhatsOn from "./WhatsOn";
+import SignUp from "./SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
@@ -16,7 +17,7 @@ function App() {
             <Route path="/comingsoon" element={<div>Coming Soon</div>} />
             <Route path="/yourvisit" element={<div>Your Visit</div>} />
             <Route path="/booktickets" element={<div>Book Tickets</div>} />
-            <Route path="/signup" element={<div>Sign Up</div>} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/whatson" element={<WhatsOn />} />
           </Routes>
           <Footer />
---
+++ src/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import joinTheClub from "./assets/images/joinTheClub.png";
+
+const SignUp = () => {
+
+    return (
+        <><div className="signup-body">
+            <img src={joinTheClub} className='signup-image' />
+            <div className='signup-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec nisi ut mi condimentum pharetra. Quisque non urna sit amet odio sollicitudin eleifend ac non ligula. Nulla facilisi. Morbi in justo vel ipsum sollicitudin rutrum. Nulla facilisi. Proin vel neque nec nulla tincidunt volutpat. Pellentesque nec augue ac ipsum dictum congue. Aliquam euismod blandit enim, in ullamcorper ipsum dictum non. Aenean ut tortor velit.
+            </div>
+        </div>
+            
+        </>
+    )
+}
+
+export default SignUp
