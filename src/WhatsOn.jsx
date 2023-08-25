fba85f9094fd5e1f0b57fc0b79be40d85959c5a4
Reuben Raj <reuben.raj@thredd.com>
2023-08-25 09:21:25 +0100

    Updated Whats On jsx

--- src/WhatsOn.jsx
+++ src/WhatsOn.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import MoviePosterDisplay from './components/MoviePosterDisplay'
 
 const WhatsOn = () => {
+    
     return (
-        <div>
-            About Us Page
+        <div> 
+            <h1>What is On</h1>
+            <div>
+                <MoviePosterDisplay title="The Pool" poster="" rating="12A" showings={['12:30', '14:00', '20:00']} summary="suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis"/>
+            </div>
         </div>
+        
     )
 }
 
