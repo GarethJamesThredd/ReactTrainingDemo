9c6681c7ca89ffda16a867f3dd0e38e2a5096d60
Reuben Raj <reuben.raj@thredd.com>
2023-08-25 09:35:38 +0100

    Updated whats on page

--- src/WhatsOn.jsx
+++ src/WhatsOn.jsx
@@ -5,7 +5,10 @@ const WhatsOn = () => {
     
     return (
         <div className='body'>
-            Whats On Page
+            <h1>Whats On Page</h1>
+            <div>
+                <MoviePosterDisplay title="The Pool" poster="assets/posters/180x260.png" rating="12A" showings={['12:30', '14:00', '20:00']} summary="suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis"/>
+            </div>
         </div>
         
     )
