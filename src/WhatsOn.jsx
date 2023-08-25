9c24254f911db8348d0e158084123d8ab866b7b7
Reuben Raj <reuben.raj@thredd.com>
2023-08-25 12:40:36 +0100

    Updates to css files

--- src/WhatsOn.jsx
+++ src/WhatsOn.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import MoviePosterDisplay from './components/MoviePosterDisplay'
+import homepageImg from './assets/images/homePage.png'
 import movieData from "./movies.json";
 
 const WhatsOn = () => {
     
     return (
-        <div className='body'>
-            <h1>Whats On Page</h1>
-            <div class="grid-container">
-                {movieData.movies.map((item) => (
-                    <MoviePosterDisplay title={item.title} poster={item.poster} url={item.url} review={item.rating} showings={item.showings} summary={item.summary}/>
-                
-                ))}
+        <div className='container'>
+            <div className='homepage'>
+                <img alt='homepageImage' src={homepageImg} id='homepageImg'/>
+            </div>
+            <div className='whats-on'>
+                <h2>What's On</h2>
+                <div className='whats-on-card-container'>
+                    {movieData.movies.map((item) => (
+                        <MoviePosterDisplay title={item.title} poster={item.poster} url={item.url} review={item.rating} showings={item.showings} summary={item.summary}/>
+                    ))}
+                    
+                </div>
             </div>
         </div>
-        
     )
 }
 
-export default WhatsOn
+export default WhatsOn;
